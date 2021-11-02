4fb63069cb5fa84c100c7c76cb107afea9bca741
AfKsenia <afksenia@gmail.com>
2021-11-02 17:56:50 +0300

    Добавьте файлы проекта.

---
+++ Lab6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку ");
+            string str = Console.ReadLine();
+            int i = 0;
+            int j = str.Length - 1;
+            bool f = true;
+            for (; i < j; i++, j--)
+                if (str[i] != str[j])
+                {
+                    f = false;
+                    break;
+                }
+                else
+                {
+                    f = true;
+                }
+            if (!f)
+                Console.WriteLine("Это не полиндром");
+            else
+                Console.WriteLine("Это полиндром");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите строку");
+            string st = Console.ReadLine();
+            string[] starray = st.Split();
+            int stlenght = starray[0].Length;
+            for (int i = 0; i < starray.Length; i++)
+            {
+                if (starray[i].Length > stlenght)
+                    stlenght = starray[i].Length;
+                Console.Write("{0} ", starray[i].Length);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Самое длинное слово состоит из {0} символов", stlenght);
+            Console.ReadKey();
+        }
+    }
+}
